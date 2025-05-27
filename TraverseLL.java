e475ba345802b829bd2b7d573d59300ac753f744
Sarthak Shahi <168637240+SarthakShahi2003@users.noreply.github.com>
2025-05-27 16:21:27 +0530

    Linked List

---
+++ TraverseLL.java
@@ -0,0 +1,36 @@
+public class TraverseLL {
+    
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data1) {
+            this.data = data1;
+            this.next = null;
+        }
+    }
+
+    private static Node traverseLL(int[] arr) {
+        Node head = new Node(arr[0]);
+        Node mover = head;
+        for (int i = 1; i < arr.length; i++) {
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+        return head;
+    }
+
+   
+    public static void main(String[] args) {
+        int[] arr = {12, 3, 5, 6, 8};
+         Node head = traverseLL(arr);
+        Node temp=head;
+        while(temp!=null){
+        System.out.print(temp.data+ " ");
+        temp=temp.next;
+        }
+    }
+}
+
+
